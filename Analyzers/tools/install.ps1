7e73b911adb3e94aadc7d0fcf14c9dcc28c72efb
Lawson Caudill <lawson@getthinktank.com>
2019-09-23 18:50:08 -0300

    Fix analyzer justification installation.

--- Analyzers/tools/install.ps1
+++ Analyzers/tools/install.ps1
@@ -11,14 +11,14 @@ $justificationsBasePath = Join-Path $analyzersFilesBasePath "Justifications.cs"
 
 $justificationsFileContents = Get-Content $justificationsBasePath
 $justificationsFileContents = $justificationsFileContents.Replace('NAMESPACETOKEN', $project.Name)
-$justificationsFileContents | Out-File $justificationsBasePath
+$justificationsFileContents | Out-File -FilePath $justificationsBasePath
 
 $project.ProjectItems.AddFolder(".analyzers") | Out-Null
 $folder = $project.ProjectItems.Item(".analyzers")
 
 $folder.ProjectItems.AddFromFileCopy($styleCopDotJsonBasePath) | Out-Null
 $folder.ProjectItems.AddFromFileCopy($caDictionaryBasePath) | Out-Null
-$folder.ProjectItems.AddFromFileCopy($justificationsFileContents) | Out-Null
+$folder.ProjectItems.AddFromFileCopy($justificationsBasePath) | Out-Null
 
 # Fix up stylecop.json file (voodoo magic courtesy of StingyJack - https://github.com/dotnet/roslyn/issues/4655)
 $item = $folder.ProjectItems.Item("$(Split-Path $styleCopDotJsonBasePath -Leaf)")
