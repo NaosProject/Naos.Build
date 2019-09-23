a725de0e6f4a5f3472c282a635a8d4d60c4a9580
Lawson Caudill <lawson@getthinktank.com>
2019-09-23 18:44:59 -0300

    Added place to manage common analysis suppression justifications.

--- Analyzers/tools/install.ps1
+++ Analyzers/tools/install.ps1
@@ -7,12 +7,18 @@ $styleCopDotJsonBasePath = Join-Path $analyzersFilesBasePath "stylecop.json"
 $caDictionaryBasePath = Join-Path $analyzersFilesBasePath "dictionary.xml"
 $releaseRulesetBasePath = Join-Path $analyzersFilesBasePath "release.ruleset"
 $testRulesetBasePath = Join-Path $analyzersFilesBasePath "test.ruleset"
+$justificationsBasePath = Join-Path $analyzersFilesBasePath "Justifications.cs"
+
+$justificationsFileContents = Get-Content $justificationsBasePath
+$justificationsFileContents = $justificationsFileContents.Replace('NAMESPACETOKEN', $project.Name)
+$justificationsFileContents | Out-File $justificationsBasePath
 
 $project.ProjectItems.AddFolder(".analyzers") | Out-Null
 $folder = $project.ProjectItems.Item(".analyzers")
 
 $folder.ProjectItems.AddFromFileCopy($styleCopDotJsonBasePath) | Out-Null
 $folder.ProjectItems.AddFromFileCopy($caDictionaryBasePath) | Out-Null
+$folder.ProjectItems.AddFromFileCopy($justificationsFileContents) | Out-Null
 
 # Fix up stylecop.json file (voodoo magic courtesy of StingyJack - https://github.com/dotnet/roslyn/issues/4655)
 $item = $folder.ProjectItems.Item("$(Split-Path $styleCopDotJsonBasePath -Leaf)")
