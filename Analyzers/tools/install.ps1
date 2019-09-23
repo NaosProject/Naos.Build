d221e11b3af1570f4696970428724baa1c64a9f9
Lawson Caudill <lawson@getthinktank.com>
2019-09-23 19:09:35 -0300

    Moved file to temp directory to avoid nuget trashing it.

--- Analyzers/tools/install.ps1
+++ Analyzers/tools/install.ps1
@@ -8,8 +8,7 @@ $caDictionaryBasePath = Join-Path $analyzersFilesBasePath "dictionary.xml"
 $releaseRulesetBasePath = Join-Path $analyzersFilesBasePath "release.ruleset"
 $testRulesetBasePath = Join-Path $analyzersFilesBasePath "test.ruleset"
 $justificationsBasePath = Join-Path $analyzersFilesBasePath "Justifications.cs"
-$justificationsBasePathCustom = Join-Path $analyzersFilesBasePath $project.Name
-$justificationsBasePathCustom = Join-Path $justificationsBasePathCustom "Justifications.cs"
+$justificationsBasePathCustom = Join-Path (Join-Path [System.IO.Path]::GetTempPath() $project.Name) "Justifications.cs"
 
 $justificationsFileContents = Get-Content $justificationsBasePath
 $justificationsFileContents = $justificationsFileContents.Replace('NAMESPACETOKEN', $project.Name)
@@ -21,6 +20,8 @@ $folder = $project.ProjectItems.Item(".analyzers")
 $folder.ProjectItems.AddFromFileCopy($styleCopDotJsonBasePath) | Out-Null
 $folder.ProjectItems.AddFromFileCopy($caDictionaryBasePath) | Out-Null
 $folder.ProjectItems.AddFromFileCopy($justificationsBasePathCustom) | Out-Null
+
+Remove-Item $justificationsBasePathCustom
 
 # Fix up stylecop.json file (voodoo magic courtesy of StingyJack - https://github.com/dotnet/roslyn/issues/4655)
 $item = $folder.ProjectItems.Item("$(Split-Path $styleCopDotJsonBasePath -Leaf)")
