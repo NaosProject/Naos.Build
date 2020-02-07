f997ef0283ffe8757cf23cbf4d181610dd707808
Lawson Caudill <lawson@getthinktank.com>
2020-02-07 14:50:23 +0000

    Updated visual studio project templates.

---
+++ Conventions/VisualStudioProjectTemplates/Serialization.Json/$secondPrefixElement$JsonConfiguration.cs
@@ -0,0 +1,23 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="$secondPrefixElement$BsonConfiguration.cs" company="Naos Project">
+//    Copyright (c) Naos Project 2019. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace $projectName$
+{
+    using System;
+    using System.Collections.Generic;
+    using OBeautifulCode.Serialization.Json;
+
+    /// <summary>
+    /// Implementation for the <see cref="$secondPrefixElement$" /> domain.
+    /// </summary>
+    public class $secondPrefixElement$JsonConfiguration : JsonConfigurationBase
+    {
+        /// <inheritdoc />
+        protected override IReadOnlyCollection<Type> TypesToAutoRegister => new[]
+        {
+        };
+    }
+}
